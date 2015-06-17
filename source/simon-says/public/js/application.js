6dc0bed8e6647e3c34314f4972f8c66f876e33f7
James Newman <jamesmnewman1@gmail.com>
2015-06-16 19:39:25 -0700

    bright colors

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-
+  $("#get_color").click(function(event){
+	event.preventDefault();
+	$.ajax({
+		url: "/color",
+		method: "POST",
+		dataType: "json",
+		success: getColors = function(data) {
+         $("#color_me li:nth-child(" + data.cell + ")").css("background-color", data["color"]);
+         console.log("success");
+        }
+	});
+  });
 });
