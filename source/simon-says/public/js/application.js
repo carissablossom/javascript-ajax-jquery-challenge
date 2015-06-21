6deb47f67a02be74190aba21b88bded795111f72
Jeff Chang <jeffrey.chang2014@gmail.com>
2015-06-20 18:08:02 -0700

    start challenge

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
 
 });
+
+var colorListener = function(){
+  $('.container').on("click", "#get_color", function(event){
+    event.preventDefault();
+
+    request = $.ajax({
+      url: '/color',
+      dataType: "JSON",
+      type: 'POST',
+
+    });
+  };
+};
