8703e657d2875aa383ea17e1f61fcbda2d2fa26d
Kelvin Gann <klvngnn@gmail.com>
2015-06-20 21:18:44 -0700

    finished challenge, learned how to select data and to pass in json objects when ajax call is finished

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
+  $('#get_color').on('click',function(event){
+    event.preventDefault();
 
+    $.ajax({
+      url: "/color",
+      method: "POST",
+      dataType: "json"
+    })
+    .done(
+      function(data){
+        $("#color_me li:nth-child(" + data.cell + ")").css("background-color", data.color);
+    })
+  })
 });
