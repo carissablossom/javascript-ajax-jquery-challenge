8d95674a9e68f387403fbe35c1f36880e92377a5
Nicholas Toulouse <faintsoundofwater@gmail.com>
2015-06-21 18:38:25 -0700

    working jquery and ajax version of colored cards

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,23 @@
+var hexcolors = ['Aqua', 'Red', 'Maroon', 'Yellow', 'Olive', 'Lime', 'Green', 'Aqua', 'Teal', 'Blue', 'Navy', 'Fuchsia', 'Purple'];
+
 $(document).ready(function(){
+
+  $('#get_color').on('click', function(event){
+    // var color = hexcolors[Math.floor(Math.random()*hexcolors.length)];
+    // var square = Math.floor((Math.random()* $('ul li').length )+1);
+    // var selector = "ul li:nth-child(" + square.toString() + ")";
+    // $(selector).css("background-color", color);
+
+    event.preventDefault();
+    $.ajax({
+      url: "/color",
+      method: "POST",
+      dataType: "json",
+      success: getColors = function(data) {
+        $("#color_me li:nth-child(" + data.cell + ")").css("background-color", data["color"]);
+        console.log("it works, hurray!")
+      }
+    });
+  });
 
 });
