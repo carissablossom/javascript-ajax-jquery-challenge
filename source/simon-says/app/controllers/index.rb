ebd6c58c46b47cfc0a5d3569568841b045ea7bde
Jennifer Jaochico <jenjao515@gmail.com>
2015-06-22 15:50:15 -0700

    grabbing random color data as json successful

--- source/simon-says/app/controllers/index.rb
+++ source/simon-says/app/controllers/index.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
@@ -7,6 +9,6 @@ post '/color' do
 
   #Create and return a JSON object with the random cell and color given below.
 
-  cell= rand(1..9)
-  color= "#" + "%06x" % (rand * 0xffffff)
+  {cell: rand(1..9),
+  color: "#" + "%06x" % (rand * 0xffffff)}.to_json
 end
--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,61 @@
 $(document).ready(function(){
-
+  changeColor();
 });
+
+var changeColor = function() {
+  $("#get_color").on("click", function(event){
+      event.preventDefault();
+
+      //form.serialize();
+      //returns data as a query string
+      //description=a&size&magic=panda
+      //name = magic, value = panda
+      //key = magic, value = panda
+      // jquery sends query string to the server instead of JSON
+
+      //everything above here is just grabbing data from the DOM
+
+      $.ajax({
+        // specifies which route you're going to extra data from
+        // data has to be a hash or a string,  you can save this as a variable
+        type: "post",
+        url: '/color',
+        dataType: 'json'
+      })
+
+      .done(function(response) {
+        // insert jquery
+        console.log(response);
+      })
+
+      .fail(function(response) {
+        console.log("Color information not found. Check code for changeColor();", response);
+      })
+  })
+
+};
+
+// var switchLogintoSignUp = function(){
+//   $("#signup").on("click", function(event){
+//       var url = $(this).attr("href");
+
+//       event.preventDefault();
+
+//       $.ajax({
+//         type: "get",
+//         url: url,
+//       })
+
+//       .done(function(response){
+//           $("#splash").append(response);
+//           $("#login-form").hide();
+//           $("#signup-form").show();
+//       })
+
+//       .error(function(response){
+//         console.log("Information not sent. Check code for SwitchLoginSignUp();", response);
+//       })
+//   })
+// };
+
+
