49c31b2fa3a9fbe3928c361b420de5161c21ac6f
David Grotting & erictflores & Vivek Poola <pair+dgrotting.erictflores.vpoola88@devbootcamp.com>
2015-06-23 11:04:39 -0700

    take 2

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
 
+    $('a').on("click", function(event){
+      event.preventDefault();
+
+      var path = "/color"
+      // var form =
+
+      request = $.ajax({
+        url: path,
+        type: "POST",
+        dataType: "JSON"
+      })
+
+      request.done(function(response){
+        $('li:nth-child('+ response.cell +')').css("background-color", response.color);
+      })
+
+      request.fail(function(response){
+        console.log(response);
+      })
+
+    })
+
 });
