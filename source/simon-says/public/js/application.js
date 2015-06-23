b3e223cbe8f822a5f668441a61c66f11afba64b9
Tanay Arora & Vivek Poola <pair+tarora2014.vpoola88@devbootcamp.com>
2015-06-22 22:13:58 -0700

    Release 0 copleted

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
+  $('a#get_color').on("click", function(event){
+    event.preventDefault();
 
+    var path = $(this)
+
+    // debugger
+    request = $.ajax({
+      url: "/color",
+      type: "POST"
+    })
+
+    request.done(function(response){
+      console.log(response);
+      $('#color_me li:nth-child('+response.cell+')').css("background-color", response.color)
+    })
+
+    request.fail(function(response){
+      console.log(response);
+      console.log("you suck");
+    })
+  })
 });
