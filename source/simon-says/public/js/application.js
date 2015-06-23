c5955b38b2f1d065039bead94457407fe5f4e692
Andrew Donato <andrew.donato91@gmail.com>
2015-06-23 13:26:05 -0700

    completed simon say with Jenga

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
-
+  simonColor();
 });
+
+var simonColor = function() {
+  $('#get_color').on('click', function(event){
+    event.preventDefault();
+
+    var url = '/color';
+    var method = 'post'
+
+    var request = $.ajax({
+      url: url,
+      type: method,
+      datatype: "json"
+      // data: ,
+    })
+
+    request.done(function(response){
+      console.log('success', response);
+      $('ul li:nth-child(' + response["cell"] + ')').css("background", response["color"])
+    })
+
+    request.fail(function(response){
+      console.log("Fail, check simonColor();", response);
+    })
+
+  })
+
+};
