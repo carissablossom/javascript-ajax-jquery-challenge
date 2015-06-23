873b2c913f2158ad7a54c511ee98bafa70828934
erictflores <eric.t.flores@gmail.com>
2015-06-23 12:45:02 -0700

    eflo take 3. feeling better about DOM manipulatin and ajax stuff

--- source/simon-says/app/controllers/index.rb
+++ source/simon-says/app/controllers/index.rb
@@ -4,9 +4,10 @@ get '/' do
 end
 
 post '/color' do
-
-  #Create and return a JSON object with the random cell and color given below.
+  content_type :json # need to tell ruby what data type this shit is
 
   cell= rand(1..9)
   color= "#" + "%06x" % (rand * 0xffffff)
+
+  {cell: cell, color: color}.to_json #makme sure to .to_json, since youre ajax call is going to interprete this and it needs jto know what data type it s
 end
--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,76 @@
 $(document).ready(function(){
-
+  clickButton();
 });
+
+
+var clickButton = function() {
+  $('#get_color').on('click', function(e){
+    e.preventDefault();
+    // alert("success");
+
+    var path = "/color";
+    var request = $.ajax({
+      url: path,
+      type: "post",
+    })
+    request.done(function(response){
+      var cell_box = $('#color_me li:nth-child('+response.cell+')');
+      cell_box.css({"background-color": response.color});
+    })
+
+    request.fail(function(error){
+      alert("your ajax call failed");
+    });
+  });
+};
+
+
+
+
+//disregard shit below. was just replicating for muscle memory
+// var clickButton = function() {
+//   $('#get_color').on('click', function(e){
+//     e.preventDefault();
+
+//     var path = "/color";
+//     var request = $.ajax({
+//       url: path,
+//       type: "post",
+//     });
+
+//     request.done(function(response){
+
+//       var cell_box = $('#color_me li:nth-child('+response.cell+')')
+//       cell_box.css({"background-color": response.color});
+//     });
+
+//     request.fail(function(error){
+//       console.log("failed");
+//       alert("ajax call failed");
+//     });
+
+//   });
+// };
+
+
+// var clickButton = function(){
+//   $('#get_color').click(function(e) {
+//     e.preventDefault();
+//     var path = "/color"
+
+//     var request = $.ajax({
+//       type: 'post',
+//       url: path,
+//     });
+
+//     request.done(function(response){
+//       var cell = $('#color_me li:nth-child('+response.cell+')');
+//       cell.css({"background-color": response.color});
+//     });
+
+//     request.fail(function(error){
+//       console.log("Your ajax request failed");
+//       alert("Your ajax request failed");
+//     });
+//   });
+// };
