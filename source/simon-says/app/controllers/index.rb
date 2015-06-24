fbc284649a61d4cfdfd4187bdcc999fbab6d1f11
Patrick S. Betti <patreek3@gmail.com>
2015-06-23 19:00:25 -0700

    add ajax call, update DOM method, doc ready functions

--- source/simon-says/app/controllers/index.rb
+++ source/simon-says/app/controllers/index.rb
@@ -4,9 +4,10 @@ get '/' do
 end
 
 post '/color' do
-
+  p "Hey, you made it."
   #Create and return a JSON object with the random cell and color given below.
-
   cell= rand(1..9)
   color= "#" + "%06x" % (rand * 0xffffff)
+  content_type :json
+  {cell: cell, color: color}.to_json
 end
--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,108 @@
-$(document).ready(function(){
 
+var getColorListener = function () {
+  $('#get_color').on('click', function(event){
+    event.preventDefault();
+    $.ajax({
+      url: "/color",
+      type: "POST"
+    })
+    .done(function(response){
+
+     updateDOM(response);
+     console.log
+    })
+
+    .fail(function(response){
+      console.log("you fucked up");
+      console.log(response);
+
+    })
+  });
+}
+
+
+var updateDOM = function(response) {
+  $('#color_me li:nth-child(' + response.cell + ')').css('background-color', response.color)
+}
+
+
+
+$(document).ready(function(){
+  getColorListener()
 });
+
+
+/*
+  VERY BROAD STROKES
+  clicking a button
+  change a random cell to a random color
+
+  broad strokes
+  clicking a button
+  prevent that buttons default
+  get random cell info from server
+  update our DOM with info from the server
+
+
+  finer points
+    clicking a button
+      - write a click event listener for "click me" button
+      -
+    prevent that buttons default
+      - click listener prevents the buttons default action
+
+    get random cell info from server
+      - AJAX request to server
+        - url: /color
+        - type: "POST"
+
+      - SERVER SIDE
+        - give us a valid response
+        - return status 200
+        - return some JSON
+        - return random cell and random color in JSON format
+
+
+    update our DOM with info from the server
+      - AJAX all good
+        - verify our data
+        - update a cell based on returned cell number with returned cell color
+
+      - AJAX tottallly f'ed
+        - verify our data
+        - alert user to failure
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
